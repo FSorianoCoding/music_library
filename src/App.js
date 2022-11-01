6cac4d5187cbeb3b5193d59edc5034af5c511fec
Fabian <fabo.soriano@gmail.com>
2022-10-31 23:02:37 -0700

    added Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import { SearchBar } from './components/SearchBar';
 import { Gallery } from './components/Gallery';
 import { useEffect, useState } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { ArtistView } from './components/ArtistView';
+import { AlbumView } from './components/AlbumView';
 
 
 function App() {
@@ -25,16 +28,14 @@ function App() {
 
   /*
   useEffect always runs when the component initially mounts to the dom.
-
   A.) when there is no dependency list
   it will run the supplied function whenever there is a change to state or props.
-
   B.) when there is an empty dependency list
   it will not run again. Ever. For any reason.
-
   C.) when there is a dependency list with elements inside
   it will run when it detects a change to the tracked variables.
   */
+
   const handleSubmit = (e, term) => {
     e.preventDefault()
     setQuery(term);
@@ -42,16 +43,22 @@ function App() {
 
   return (
     <>
-      <SearchBar handleSubmit={handleSubmit}/>
       {message}
-      <Gallery data={data}/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <SearchBar handleSubmit={handleSubmit} />
+              <Gallery data={data} />
+            </>
+          } />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
+
 export default App;
-
-
-
-
-
